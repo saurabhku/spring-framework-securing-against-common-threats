d55ed3366baaebf848a00371d438dffceb1b7ed8
saurabhkumar <saurabh.techie87@gmail.com>
2021-11-07 21:48:33 +0000

    creating a new security configuration

--- module_3/demo/src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ module_3/demo/src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -1,2 +1,16 @@
-package com.example.demo.configuration;public class SecurityConfiguration {
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic();
+
+    }
 }
