87b4a03d676b4a1b85f357db37637fc4c48028f2
saurabhkumar <saurabh.techie87@gmail.com>
2021-11-07 22:16:14 +0000

    ignore security from jars and css

--- module_3/demo/src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ module_3/demo/src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.demo.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -9,8 +10,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().anyRequest().authenticated()
-                .and().httpBasic();
+        http.authorizeRequests()
+                .mvcMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login");
+    }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/css/**","/webjars/**");
     }
 }
--- module_3/demo/src/main/java/com/example/demo/controllers/PortfolioController.java
+++ module_3/demo/src/main/java/com/example/demo/controllers/PortfolioController.java
@@ -10,4 +10,9 @@ public class PortfolioController {
 	public String portfolio() {
 		return "/portfolio";
 	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
 }
